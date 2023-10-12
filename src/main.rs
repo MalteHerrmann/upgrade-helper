04b892a68af24e1ce3139e6ec7c1b1651304e13c
MalteHerrmann <malte@evmos.org>
2023-10-12 10:42:10 +0200

    add dummy implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,98 @@
+use std::process::Command;
+
+// Enum to represent different network options
+#[derive(Debug)]
+enum Network {
+    LocalNode,
+    Testnet,
+    Mainnet,
+}
+
+// Trait to define the interface
+trait UpgradeHelper {
+    const DEFAULT_HOME: &'static str;
+    fn prepare_submit_proposal_cmd(&self);
+    fn check_target_version(&self, network: &Network);
+}
+
+struct MyUpgradeHelper {
+    target_version: String,
+}
+
+impl UpgradeHelper for MyUpgradeHelper {
+    const DEFAULT_HOME: &'static str = "/path/to/default/home";
+
+    fn prepare_submit_proposal_cmd(&self) {
+        let cmd = "evmosd tx gov submit-legacy-proposal";
+        println!("Executing command: {}", cmd);
+        // Execute the command in a shell process
+        let _result = Command::new("sh")
+            .arg("-c")
+            .arg(cmd)
+            .status()
+            .expect("Failed to execute command");
+    }
+
+    fn check_target_version(&self, network: &Network) {
+        match network {
+            Network::LocalNode => {
+                println!("Checking target version for LOCAL_NODE");
+                // Check target version logic for LOCAL_NODE
+            }
+            Network::Testnet => {
+                println!("Checking target version for TESTNET");
+                // Check target version logic for TESTNET
+            }
+            Network::Mainnet => {
+                println!("Checking target version for MAINNET");
+                // Check target version logic for MAINNET
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let helper = MyUpgradeHelper {
+        target_version: String::from("some_target_version"),
+    };
+
+    helper.prepare_submit_proposal_cmd();
+
+    let network = Network::Testnet;
+    helper.check_target_version(&network);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_check_target_version_local_node() {
+        let helper = MyUpgradeHelper {
+            target_version: String::from("some_target_version"),
+        };
+        let network = Network::LocalNode;
+        helper.check_target_version(&network);
+        // Add assertions to validate the behavior for LOCAL_NODE
+    }
+
+    #[test]
+    fn test_check_target_version_testnet() {
+        let helper = MyUpgradeHelper {
+            target_version: String::from("some_target_version"),
+        };
+        let network = Network::Testnet;
+        helper.check_target_version(&network);
+        // Add assertions to validate the behavior for TESTNET
+    }
+
+    #[test]
+    fn test_check_target_version_mainnet() {
+        let helper = MyUpgradeHelper {
+            target_version: String::from("some_target_version"),
+        };
+        let network = Network::Mainnet;
+        helper.check_target_version(&network);
+        // Add assertions to validate the behavior for MAINNET
+    }
 }
