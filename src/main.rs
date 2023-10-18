dfec436207285e344c0c5121a7b67b8f6f921810
MalteHerrmann <malte@evmos.org>
2023-10-18 10:42:51 +0200

    implement network selection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::process;
+
+use inquire::Select;
+
 // Enum to represent different network options
 #[derive(Debug)]
 enum Network {
@@ -9,37 +13,71 @@ enum Network {
 // Trait to define the interface
 trait UpgradeHelper {
     const DEFAULT_HOME: &'static str;
-    fn check_target_version(&self, network: &Network);
+    fn check_target_version(&self);
 }
 
-struct MyUpgradeHelper {}
+struct MyUpgradeHelper {
+    network: Network,
+}
 
 impl UpgradeHelper for MyUpgradeHelper {
     const DEFAULT_HOME: &'static str = "/path/to/default/home";
 
-    fn check_target_version(&self, network: &Network) {
-        match network {
+    fn check_target_version(&self) {
+        match self.network {
             Network::LocalNode => {
                 println!("Checking target version for LOCAL_NODE");
                 // Check target version logic for LOCAL_NODE
-            }
+            },
             Network::Testnet => {
                 println!("Checking target version for TESTNET");
                 // Check target version logic for TESTNET
-            }
+            },
             Network::Mainnet => {
                 println!("Checking target version for MAINNET");
                 // Check target version logic for MAINNET
-            }
+            },
         }
     }
 }
 
 fn main() {
-    let helper = MyUpgradeHelper {};
+    let network_options = vec![
+        "Local Node",
+        "Testnet",
+        "Mainnet",
+    ];
 
-    let network = Network::Testnet;
-    helper.check_target_version(&network);
+    // Initialize the network variable
+    let used_network: Network;
+
+    // Prompt the user to select the network
+    let chosen_network = Select::new("Select network", network_options).prompt();
+    match chosen_network {
+        Ok(choice) => {
+            match choice {
+                "Local Node" => used_network = Network::LocalNode,
+                "Testnet" => used_network = Network::Testnet,
+                "Mainnet" => used_network = Network::Mainnet,
+                &_ => {
+                    println!("Invalid network selected: {:?}", choice);
+                    process::exit(1);
+                },
+            }
+        }
+        Err(e) => { 
+            println!("Error selecting network: {}", e); 
+            process::exit(1);
+        }
+    }
+
+    // Create an instance of the helper
+    let helper = MyUpgradeHelper {
+        network: used_network,
+    };
+
+    // Check the target version
+    helper.check_target_version();
 }
 
 #[cfg(test)]
@@ -48,25 +86,25 @@ mod tests {
 
     #[test]
     fn test_check_target_version_local_node() {
-        let helper = MyUpgradeHelper {};
         let network = Network::LocalNode;
-        helper.check_target_version(&network);
+        let helper = MyUpgradeHelper {network};
+        helper.check_target_version();
         // Add assertions to validate the behavior for LOCAL_NODE
     }
 
     #[test]
     fn test_check_target_version_testnet() {
-        let helper = MyUpgradeHelper {};
         let network = Network::Testnet;
-        helper.check_target_version(&network);
+        let helper = MyUpgradeHelper {network};
+        helper.check_target_version();
         // Add assertions to validate the behavior for TESTNET
     }
 
     #[test]
     fn test_check_target_version_mainnet() {
-        let helper = MyUpgradeHelper {};
         let network = Network::Mainnet;
-        helper.check_target_version(&network);
+        let helper = MyUpgradeHelper {network};
+        helper.check_target_version();
         // Add assertions to validate the behavior for MAINNET
     }
 }
