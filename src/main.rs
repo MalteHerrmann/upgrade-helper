58cc9a825fd0a9ead8e9a54207e9e9d545804a3a
MalteHerrmann <malte@evmos.org>
2023-10-21 10:16:14 +0200

    move version utils to own module

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,38 @@ mod inputs;
 mod network;
 mod release;
 mod proposal;
+mod version;
 
 use std::process;
 
 fn main() {
-    // Prompt the user for the necessary input
+    // Query and check the network to use
     let used_network = inputs::get_used_network();
-    let target_version = inputs::get_target_version();
+
+    // Query and check the version to upgrade from
+    let previous_version = inputs::get_text("Previous version to upgrade from:");
+    let valid_version = version::is_valid_version(previous_version.as_str());
+    if !valid_version {
+        println!("Invalid previous version: {}", previous_version);
+        process::exit(1);
+    }
+
+    // Query and check the target version to upgrade to
+    let target_version = inputs::get_text("Target version to upgrade to:");
+    let valid_version = version::is_valid_target_version(
+        used_network, target_version.as_str(),
+    );
+    if !valid_version {
+        println!("Invalid target version for {}: {}", used_network, target_version);
+        process::exit(1);
+    }
 
     // Create an instance of the helper
     let upgrade_helper = helper::UpgradeHelper::new(
-        used_network, target_version.as_str(),
+        used_network,
+        previous_version.as_str(),
+        target_version.as_str(),
     );
-
-    // Check the target version
-    let valid_version = upgrade_helper.check_target_version();
-    if !valid_version {
-        process::exit(1);
-    }
 
     // Check if release was already created
     let release_exists = release::check_release_exists(upgrade_helper.target_version.as_str());
