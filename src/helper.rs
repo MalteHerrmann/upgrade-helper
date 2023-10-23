4dff4b160d807df2d2bd57f103a76717e56f8b30
MalteHerrmann <malte@evmos.org>
2023-10-23 22:56:40 +0200

    add voting period for upgrade helper

--- src/helper.rs
+++ src/helper.rs
@@ -1,10 +1,12 @@
 use crate::network::Network;
+use chrono::Duration;
 
 pub struct UpgradeHelper {
     pub network: Network,
     pub previous_version: String,
     pub target_version: String,
     pub proposal_name: String,
+    pub voting_period: Duration,
 }
 
 impl UpgradeHelper {
@@ -16,11 +18,19 @@ impl UpgradeHelper {
     ) -> UpgradeHelper {
         let proposal_name = format!("Evmos {} {} Upgrade", network, target_version);
 
+        let voting_period: Duration;
+        match network {
+            Network::LocalNode => voting_period = Duration::hours(1),
+            Network::Testnet => voting_period = Duration::hours(12),
+            Network::Mainnet => voting_period = Duration::hours(120),
+        }
+
         UpgradeHelper {
             network,
             previous_version: previous_version.to_string(),
             target_version: target_version.to_string(),
             proposal_name,
+            voting_period,
         }
     }
 }
