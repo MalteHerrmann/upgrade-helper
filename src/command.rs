5e0d73d622d89488a4429248e11015cc045c51a5
MalteHerrmann <malte@evmos.org>
2023-11-28 21:50:10 +0100

    add key and rpc urls for command

--- src/command.rs
+++ src/command.rs
@@ -3,6 +3,7 @@ use crate::release::{get_asset_string, get_release};
 use handlebars::{Handlebars, RenderError};
 use serde_json::json;
 use std::process;
+use crate::network::Network;
 
 /// Prepares the command to submit the proposal using the Evmos CLI.
 pub async fn prepare_command(helper: &UpgradeHelper) -> Result<String, RenderError> {
@@ -24,9 +25,9 @@ pub async fn prepare_command(helper: &UpgradeHelper) -> Result<String, RenderErr
 
     let assets = get_asset_string(&release).await;
     let description = "TODO: description";
-    let fees = "TODO: fees";
-    let key = "TODO: key";
-    let tm_rpc = "TODO: tm_rpc";
+    let fees = "10000000000aevmos";
+    let key = get_key(helper.network);
+    let tm_rpc = get_rpc_url(helper.network);
 
     let data = json!({
         "assets": assets,
@@ -44,6 +45,24 @@ pub async fn prepare_command(helper: &UpgradeHelper) -> Result<String, RenderErr
     handlebars.render("command", &data)
 }
 
+/// Returns the key used for signing based on the network.
+fn get_key(network: Network) -> String {
+    match network {
+        Network::Mainnet => "mainnet-address".to_string(),
+        Network::Testnet => "testnet-address".to_string(),
+        Network::LocalNode => "dev0".to_string(),
+    }
+}
+
+/// Returns the RPC URL based on the network.
+fn get_rpc_url(network: Network) -> String {
+    match network {
+        Network::Mainnet => "https://tm.evmos.lava.build:26657".to_string(),
+        Network::Testnet => "https://tm.evmos-testnet.lava.build:26657".to_string(),
+        Network::LocalNode => "http://localhost:26657".to_string(),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -58,7 +77,31 @@ mod tests {
             .await
             .expect("Failed to prepare command");
 
-        let expected_command = format!("evmosd tx gov submit-proposal software-upgrade Test Proposal v14.0.0 --description \"This is a test proposal.\" --upgrade-height 1000 --upgrade-time 1000 --voting-period 1000 --from evmos --keyring-backend test --chain-id evmos_9000-1 --home {}/.tmp-evmosd --fees 10000000aevmos --node tcp://localhost:26657 --yes", helper.home.as_os_str().to_str().unwrap());
+        let expected_command = format!("evmosd tx gov submit-legacy-proposal software-upgrade v14.0.0 --description \"This is a test proposal.\" --upgrade-height 1000 --from testnet-address --keyring-backend test --chain-id evmos_9000-1 --home {}/.tmp-evmosd --fees 10000000aevmos --node https://tm.evmos-testnet.lava.build:26657", helper.home.as_os_str().to_str().unwrap());
         assert_eq!(command, expected_command, "command does not match");
+    }
+
+    #[test]
+    fn test_get_key() {
+        let key = get_key(Network::Mainnet);
+        assert_eq!(key, "mainnet-address", "key does not match");
+
+        let key = get_key(Network::Testnet);
+        assert_eq!(key, "testnet-address", "key does not match");
+
+        let key = get_key(Network::LocalNode);
+        assert_eq!(key, "dev0", "key does not match");
+    }
+
+    #[test]
+    fn test_get_rpc_url() {
+        let rpc = get_rpc_url(Network::Mainnet);
+        assert_eq!(rpc, "https://tm.evmos.lava.build:26657", "rpc does not match");
+
+        let rpc = get_rpc_url(Network::Testnet);
+        assert_eq!(rpc, "https://tm.evmos-testnet.lava.build:26657", "rpc does not match");
+
+        let rpc = get_rpc_url(Network::LocalNode);
+        assert_eq!(rpc, "http://localhost:26657", "rpc does not match");
     }
 }
